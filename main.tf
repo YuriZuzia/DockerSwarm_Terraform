ebd5ffc400c625055e086ab9dcbe34d8d080f64c
YuriZuzia <teanjam@ya.ru>
2023-06-04 00:26:53 +0500

    working good

--- main.tf
+++ main.tf
@@ -13,9 +13,11 @@ provider "yandex" {
   folder_id = "${var.folder_id}"
   zone = "${var.zone}"
 }
+
 resource "yandex_compute_instance" "srv1" {
-  description = "D1.7 home work"
+  description = "Docker Swarm cluster manager"
   name = "srv1"
+  hostname = "srv1"
   platform_id = "standard-v1"
   zone = "${var.zone}"
 
@@ -38,10 +40,55 @@ resource "yandex_compute_instance" "srv1" {
     nat = true
   }
   metadata = {
-    serial-port-enable = 0
-    ssh-keys = "yuri:${file(var.ssh-path)}"
+    ssh-keys = "yuri:${file(var.ssh-path)}" //ssh-path described in separate secrets.tf file
+    user-data = <<EOF
+#!/bin/bash
+docker run -d --name=dev-consul -p 8500:8500 -e CONSUL_BIND_INTERFACE=eth0 consul
+docker swarm init --advertise-addr $(ip -br a show eth0 | awk '{print$3}'|cut -d'/' -f1)
+worker_key=$(docker swarm join-token -q worker)
+curl --request PUT --data $worker_key http://localhost:8500/v1/kv/worker_key
+EOF
   }
 }
 locals {
   external_ip = ["${yandex_compute_instance.srv1.network_interface.0.nat_ip_address}"]
+}
+output "locals" {
+  value = local.external_ip
+}
+
+
+resource "yandex_compute_instance" "worker" {
+  description = "Docker Swarm cluster members"
+  count = 2
+  hostname = "wrk${count.index + 1}"
+  name = "wrk${count.index + 1}"
+  platform_id = "standard-v1"
+  zone = "${var.zone}"
+  resources {
+    core_fraction = 20
+    cores = 2
+    memory = 2
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "${data.yandex_compute_image.container-optimized-image.id}"
+    }
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.int-net1.id}"
+    nat = true
+  }
+  metadata = {
+    ssh-keys = "yuri:${file(var.ssh-path)}"
+    user-data = <<EOF
+#!/bin/bash
+worker_key=$(curl -s http://srv1:8500/v1/kv/worker_key?raw)
+docker swarm join --token $worker_key srv1:2377
+EOF
+  }
+  depends_on = [ yandex_compute_instance.srv1 ]
 }
--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "zone" {
 variable "os-image-id" {
   description = "container-optimized-image id is fd80o2eikcn22b229tsa"
   type = string
-  default = "$(data.yandex_compute_image.container-optimized-image.id)"
+  default = "data.yandex_compute_image.container-optimized-image.id"
 }
 data "yandex_compute_image" "container-optimized-image" {
     family = "container-optimized-image"
